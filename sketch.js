abb2495912df355b0e79afff587a23de23c1d5c6
wanyuchiu0907 <75353952+wanyuchiu0907@users.noreply.github.com>
2020-12-02 14:02:13 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://wanyuchiu0907.github.io/20201202frog/model.json';
 
 
 function preload() {
